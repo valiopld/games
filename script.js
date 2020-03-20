510253d955c585749eb572447f0a538b3870d51e
Valentin <52473395+valiopld@users.noreply.github.com>
2020-03-20 23:21:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-window.innerWidth < 1200 ? error() : open();
+document.querySelector(".mainContainer").addEventListener("click", a => {
+  if (a.target.className == "game") {
+    let target = a.target;
+    let elses = [];
 
-function open() {
-  document.querySelector(".mainContainer").addEventListener("click", a => {
-    if (a.target.className == "game") {
-      let target = a.target;
-      let elses = [];
+    document.querySelector(".visible").style.opacity = "0.2";
+    document.querySelector(".openedGame").style.opacity = "1";
+    document.querySelector(".openedGame").style.zIndex = "99";
 
-      document.querySelector(".visible").style.opacity = "0.2";
-      document.querySelector(".openedGame").style.opacity = "1";
-      document.querySelector(".openedGame").style.zIndex = "99";
+    let iframe = document.querySelector("iframe");
+    let idGame = target.id;
+    iframe.setAttribute("src", "./games/" + idGame + ".html");
+  }
+});
 
-      let iframe = document.querySelector("iframe");
-      let idGame = target.id;
-      iframe.setAttribute("src", "./games/" + idGame + ".html");
-    }
-  });
-
-  document.querySelector(".exit").addEventListener("click", a => {
-    document.querySelector(".visible").style.opacity = "1";
-    document.querySelector(".openedGame").style.opacity = "0";
-    document.querySelector(".openedGame").style.zIndex = "-1";
-  });
-}
-
-function error() {
-  console.log("This page is avalable on screen bigger than 1200px");
-  document.querySelector(".visible").style.display = "none";
-  document.querySelector(".openedGame").style.display = "none";
-  document.querySelector(".error").style.display = "block";
-}
+document.querySelector(".exit").addEventListener("click", a => {
+  document.querySelector(".visible").style.opacity = "1";
+  document.querySelector(".openedGame").style.opacity = "0";
+  document.querySelector(".openedGame").style.zIndex = "-1";
+});
